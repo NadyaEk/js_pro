b2138fc46d0a8c567b97a2a3fa548ab26ebbae9b
Nadya <nadya@ekimenko.ru>
2020-10-09 23:16:18 +0300

    modified main.js modified index.html created style.sass style.css.map style.css

--- Lesson1/project/js/main.js
+++ Lesson1/project/js/main.js
@@ -1,11 +1,26 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
+const products = [{
+    id: 1,
+    title: 'Notebook',
+    price: 20000
+  },
+  {
+    id: 2,
+    title: 'Mouse',
+    price: 1500
+  },
+  {
+    id: 3,
+    title: 'Keyboard',
+    price: 5000
+  },
+  {
+    id: 4,
+    title: 'Gamepad',
+    price: 4500
+  },
 ];
 
-const renderProduct = (title, price, img = '') => {
+const renderProduct = (title, price, img = 'https://via.placeholder.com/150') => {
   return `<div class="product-item">
             <img src="${img}" alt="">
             <h3>${title}</h3>
@@ -17,7 +32,8 @@ const renderProduct = (title, price, img = '') => {
 const renderProducts = (list) => {
   const productList = list.map(item => renderProduct(item.title, item.price));
   console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+  let [a, b, c, d] = productList;
+  document.querySelector('.products').innerHTML = `${a} ${b} ${c} ${d}`;
 };
 
 renderProducts(products);
