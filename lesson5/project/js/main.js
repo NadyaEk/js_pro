f7d5f9aae332e55f10aee16801f01caf384bd450
Nadya <nadya@ekimenko.ru>
2020-10-29 15:52:48 +0300

    changed index.html and main.js

--- lesson5/project/js/main.js
+++ lesson5/project/js/main.js
@@ -5,7 +5,14 @@ const app = new Vue({
     data: {
         catalogUrl: '/catalogData.json',
         products: [],
+        filtered: [],
         imgCatalog: 'https://placehold.it/200x150',
+        imgCart: 'https://placehold.it/50x50',
+        cartUrl: '/getBasket.json',
+        productsCart:[],
+        isVisibleCart:'display: none',
+        show: false,
+        searchLine: 'search.value',
     },
     methods: {
         getJson(url) {
@@ -18,6 +25,23 @@ const app = new Vue({
         addProduct(product) {
             console.log(product.id_product)
         },
+        init(container) {
+
+
+        },
+        filter(searchLine){
+            const regexp = new RegExp(searchLine, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.product_name));
+
+    //         this.allProducts.forEach(el => {
+    //         const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+    //         if(!this.filtered.includes(el)){
+    //           block.classList.add('invisible');
+    //             } else {
+    //          block.classList.remove('invisible');
+    //   }
+    // })
+  }
     },
     beforeCreate() {
         console.log('beforeCreate');
@@ -30,18 +54,28 @@ const app = new Vue({
                     this.products.push(el);
                 }
             });
+        this.getJson(`${API}${this.cartUrl}`)
+            .then(data => {
+                for (el of data.contents) {
+                    this.productsCart.push(el);
+                }
+            });
     },
     beforeMount() {
         console.log('beforeMount');
+        // this.filter(value);
     },
     mounted() {
         console.log('mounted');
+
     },
     beforeUpdate() {
         console.log('beforeUpdate');
+        // this.filter(value);
     },
     updated() {
         console.log('updated');
+        this.filter(value);
     },
     beforeDestroy() {
         console.log('beforeDestroy');
