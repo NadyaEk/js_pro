51dd50fa274f2c717e4137bc9d13a73b9b05d240
Nadya <nadya@ekimenko.ru>
2020-11-05 16:46:43 +0300

    feature in statistic.js

--- lesson7/project_express/server/cart.js
+++ lesson7/project_express/server/cart.js
@@ -1,53 +1,27 @@
-// const stat = require('./statistic');;
+const stat = require('./statistic');;
 const fs = require('fs');
 
 const add = (cart, req) => {
   cart.contents.push(req.body);
   cart.countGoods++;
-  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
-  if (!err) {
-    var stats = JSON.parse(data); 
-    stats.contents.push({'action':'add','title':`${req.body.product_name}`,'date':`${new Date()}`});
-    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
-  })
-  }
-});
-  // stat(cart, 'add','./server/db/stats.json');
+  stat(req.body, 'add');
   return JSON.stringify(cart, null, 4);
 };
 const change = (cart, req) => {
   const find = cart.contents.find(el => el.id_product === +req.params.id);
   find.quantity += req.body.quantity;
-
-  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
-  if (!err) {
-    var stats = JSON.parse(data); 
-    stats.contents.push({'action':'add','title':`${find.product_name}`,'date':`${new Date()}`});
-    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
-  })
-  }
-});
-  // stat(cart, 'plus 1','./server/db/stats.json');
+  stat(find, 'add');
   return JSON.stringify(cart, null, 4);
 };
 const del = (cart, req) => {
   const find = cart.contents.find(el => el.id_product === +req.params.id);
   if (find.quantity > 1) {
     find.quantity -= req.body.quantity;
-    // stat(cart, 'minus 1','./server/db/stats.json');
   } else {
     cart.contents.splice(cart.contents.indexOf(find), 1);
     cart.countGoods--;
-    // stat(cart, 'delete','./server/db/stats.json');
   };
-  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
-  if (!err) {
-    var stats = JSON.parse(data); 
-    stats.contents.push({'action':'delete','title':`${find.product_name}`,'date':`${new Date()}`});
-    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
-  })
-  }
-});
+  stat(find, 'delete');
   return JSON.stringify(cart, null, 4);
 };
 module.exports = {
--- lesson7/project_express/server/statistic.js
+++ lesson7/project_express/server/statistic.js
@@ -1,23 +1,14 @@
 const fs = require('fs');
 // const stats = require('./server/db/stats.json');
 
-const statist = (req, res, cart, action, stats) => {
-  fs.readFile(stats, 'utf-8', (err, data) => {
-    if (err) {
-      res.sendStatus(404, JSON.stringify({result: 0, text: err}));
-    } else {
-      const newStat = (JSON.parse(data), req);
-      newStat.action = action;
-      newStat.title = cart.contents.product_name;
-      newStat.date = newDate();
-      fs.writeFile(stats.push([...newStat]), (err) => {
-        if (err) {
-          res.send('{"result": 0}');
-        } else {
-          res.send('{"result": 1}');
-        }
-      })
-    }
-  });
+const statist = (cart, action) => {
+  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
+  if (!err) {
+    var stats = JSON.parse(data); 
+    stats.contents.push({'action':`${action}`,'title':`${cart.product_name}`,'date':`${new Date()}`});
+    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
+  })
+  }
+});
 };
 module.exports = statist;
