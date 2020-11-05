2185c415347ac58f7307a90736bb629ba1d78bb3
Nadya <nadya@ekimenko.ru>
2020-11-05 16:32:56 +0300

    finished statistic feature

--- lesson7/project_express/server/cart.js
+++ lesson7/project_express/server/cart.js
@@ -6,9 +6,9 @@ const add = (cart, req) => {
   cart.countGoods++;
   fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
   if (!err) {
-    var stats = JSON.parse(data);
-    stats.contents.push({'action':'add','title':`${cart.contents.indexOf(req.body).product_name}`,'date':`${new Date()}`});
-    fs.writeFile('./server/db/stats.json', JSON.stringify(stats), (err) => {
+    var stats = JSON.parse(data); 
+    stats.contents.push({'action':'add','title':`${req.body.product_name}`,'date':`${new Date()}`});
+    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
   })
   }
 });
@@ -18,6 +18,15 @@ const add = (cart, req) => {
 const change = (cart, req) => {
   const find = cart.contents.find(el => el.id_product === +req.params.id);
   find.quantity += req.body.quantity;
+
+  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
+  if (!err) {
+    var stats = JSON.parse(data); 
+    stats.contents.push({'action':'add','title':`${find.product_name}`,'date':`${new Date()}`});
+    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
+  })
+  }
+});
   // stat(cart, 'plus 1','./server/db/stats.json');
   return JSON.stringify(cart, null, 4);
 };
@@ -26,12 +35,19 @@ const del = (cart, req) => {
   if (find.quantity > 1) {
     find.quantity -= req.body.quantity;
     // stat(cart, 'minus 1','./server/db/stats.json');
-    //return JSON.stringify(cart, null, 4);
   } else {
     cart.contents.splice(cart.contents.indexOf(find), 1);
     cart.countGoods--;
     // stat(cart, 'delete','./server/db/stats.json');
   };
+  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
+  if (!err) {
+    var stats = JSON.parse(data); 
+    stats.contents.push({'action':'delete','title':`${find.product_name}`,'date':`${new Date()}`});
+    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
+  })
+  }
+});
   return JSON.stringify(cart, null, 4);
 };
 module.exports = {
