797d0c308198c724220e4c76d18e17e22b83949d
Nadya <nadya@ekimenko.ru>
2020-11-11 23:51:14 +0300

    added new files in /server and /public

---
+++ lesson8/src/server/statistic.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const moment = require('moment');
+
+const statist = (cart, action) => {
+  fs.readFile('./server/db/stats.json', 'utf-8', (err, data) => {
+  if (!err) {
+    var stats = JSON.parse(data); 
+    stats.push({
+        'action':`${action}`,
+        'title':`${cart.product_name}`,
+        'date':`${new Date()}`
+    });
+    fs.writeFile('./server/db/stats.json', JSON.stringify(stats, null, 4), (err) => {
+  })
+  }
+});
+};
+module.exports = statist;
